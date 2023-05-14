493274ecc54ecbf4a18096b0d8606438cd7a1083
Rohitash Singh <rohitashmtr2002@gmail.com>
2023-05-14 07:39:41 +0530

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,14 +1,31 @@
 <?php
-      
-    // When the submit button is clicked
-    if (isset($_POST['submit'])) {
-      
-        // Creating variables and 
-        // storing values in it
-        $name = $_POST['Name'];
-        $email = $_POST['Email'];
-        $msg = $_POST['Message'];
-  
-        echo "<h1><i> Good Morning, $name $email $msg </i></h1>";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Set the recipient email address
+    $to = 'recipient@example.com';
+
+    // Set the email subject
+    $subject = 'New Contact Form Submission';
+
+    // Set the email headers
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+    // Compose the email content
+    $emailContent = "Name: $name<br>";
+    $emailContent .= "Email: $email<br>";
+    $emailContent .= "Message: $message<br>";
+
+    // Send the email
+    if (mail($to, $subject, $emailContent, $headers)) {
+        echo "Thank you for your message. We'll get back to you shortly.";
+    } else {
+        echo "Oops! An error occurred while sending the email.";
     }
+}
 ?>
